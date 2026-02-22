2e9f81bf84300e483b7eed27baa5f771ce4d8c19
AlannaFM <alannamachado84@gmail.com>
2026-02-22 17:18:35 -0300

    feat: implementa funcionalidade de upload com react-dropzone

--- app/routes/upload.tsx
+++ app/routes/upload.tsx
@@ -1,11 +1,29 @@
 import {type FormEvent, useState} from "react";
 import Navbar from "~/componentes/Navbar";
+import FileUploader from "~/componentes/FileUploader";
 
 const Upload: () => React.JSX.Element = () => {
     const [isProcessing, setIsProcessing] = useState(false);
     const [statusText, setStatusText] = useState('');
+    const [file, setFile] = useState<File | null>(null);
+
+    const handleFileSelect = (file: File | null): void => {
+        setFile(file)
+    }
 
     const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const form = e.currentTarget.closest('form');
+        if (!form) return;
+        const formData = new FormData(form); //extrair detalhes como nome da empresa, descrição da vaga...
+
+        const companyName = formData.get('company-name') as string;
+        const jobTitle = formData.get('job-title') as string;
+        const jobDescription = formData.get('job-description') as string;
+
+        console.log ({
+            companyName, jobTitle, jobDescription, file
+        })
 
     }
 
@@ -27,19 +45,20 @@ const Upload: () => React.JSX.Element = () => {
                         <form id="uploaf-form" onSubmit={handleSubmit} className="flex flex-col gap-4 mt-8">
                             <div className="form-div">
                                 <label htmlFor="company-name">Nome da Empresa</label>
-                                <input type="text" name="company-name" placeholder="Nome da Empresa" id="company-name" />
+                                <input type="text" name="company-name" placeholder="Nome da Empresa" id="company-name"/>
                             </div>
                             <div className="form-div">
                                 <label htmlFor="job-title">Cargo</label>
-                                <input type="text" name="job-title" placeholder="Cargo" id="job-title" />
+                                <input type="text" name="job-title" placeholder="Cargo" id="job-title"/>
                             </div>
                             <div className="form-div">
                                 <label htmlFor="job-description">Descrição da vaga</label>
-                                <textarea rows={5} name="job-description" placeholder="Descrição da vaga" id="job-description" />
+                                <textarea rows={5} name="job-description" placeholder="Descrição da vaga"
+                                          id="job-description"/>
                             </div>
                             <div className="form-div">
                                 <label htmlFor="uploader">Upload do currículo</label>
-                                <div>Uploader</div>
+                                <FileUploader onFileSelect={handleFileSelect}/>
                             </div>
 
                             <button className="primary-button" type="submit"> Analise o currículo</button>
@@ -52,3 +71,5 @@ const Upload: () => React.JSX.Element = () => {
 }
 
 export default Upload
+
+
